42a2c77dfe5e114bff3b5723711ab52c2de25e54
Shiv Shankar <shiv.shankar3@wipro.com>
2021-05-16 14:03:14 +0530

    updated

--- JUnitSpring/src/test/java/com/example/demo/DemoApplicationTests.java
+++ JUnitSpring/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testExample() {
+		Assert.isTrue(true,"msg");
 	}
 
 }
